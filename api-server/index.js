a0d8e4252608168ff377dcc3f1cd49fc853f9f87
prabathshalitha <prabathshalitha21@gmail.com>
2025-02-26 21:13:14 +0530

    Added find max endpoint

--- api-server/index.js
+++ api-server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { findMin } =  require('./util.js');
+const { findMin, findMax } =  require('./util.js');
 const app = new express();
 const port = 3000;
 
@@ -18,6 +18,16 @@ app.get('/number/min', (req, res) => {
     let result = findMin(num1, num2);
     res.json(result);
 });
+
+// 2 - find max number endpoint
+app.get('/number/max', (req, res) => {
+    const num1 = parseFloat(req.query.num1);
+    const num2 = parseFloat(req.query.num2);
+
+    let result = findMax(num1, num2);
+    res.json(result);
+});
+
 
 app.listen(port, () => {
     console.log(`Server running on port: ${port}`);
--- api-server/util.js
+++ api-server/util.js
@@ -1,3 +1,4 @@
+// 1 - function for find min number
 function findMin(num1, num2) {
     if (Number.isNaN(num1) || Number.isNaN(num2)) {
         return ({error: "Please enter valid numbers"});
@@ -12,4 +13,21 @@ function findMin(num1, num2) {
     }
 }
 
-module.exports = { findMin };
+// 2 - function for find max number
+function findMax(num1, num2) {
+    if (Number.isNaN(num1) || Number.isNaN(num2)) {
+        return ({error: "Please enter valid numbers"});
+    }
+    else {
+        if (num1 > num2) {
+            return ({ans: `Number 1 is the max: ${num1}`})
+        }
+        else {
+            return ({ans: `Number 2 is the max: ${num2}`})
+        }
+    }
+}
+
+
+// exporting functions
+module.exports = { findMin, findMax };
